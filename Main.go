45c0b0e5a153b4a33811e0053332e7d2fce7156c
Manuel Riezebosch <mriezebosch@gmail.com>
2019-04-14 21:48:33 +0200

    feat(read object): read object from git objects

--- Main.go
+++ Main.go
@@ -1,4 +1,31 @@
-package Main
+package main
 
-func Main() {
+import (
+	"bytes"
+	"compress/zlib"
+	"io/ioutil"
+)
+
+func main() {
+}
+
+func ReadObject(filename string) (string, error) {
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	b := bytes.NewReader(dat)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r)
+
+	r.Close()
+	content := buf.String()
+
+	return content, nil
 }
--- Main_test.go
+++ Main_test.go
@@ -1,25 +1,16 @@
-package Main
+package main
 
 import (
-	"bytes"
-	"compress/zlib"
-	"io"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	dat, err := ioutil.ReadFile(".git/objects/26/faf771e5f7c50d00a03b47e71b8df52ff8a7a7")
+	content, err := ReadObject(".git/objects/ae/710a6ef6cd3145a5366d1ed2b2d918d529e88a")
 	if err != nil {
 		t.Error(err)
 	}
 
-	b := bytes.NewReader(dat)
-	r, err := zlib.NewReader(b)
-	if err != nil {
-		t.Error(err)
+	if content != "commit 252\000tree 8709fadf421ff79bea117e1195277253074d9bc8\nparent 26faf771e5f7c50d00a03b47e71b8df52ff8a7a7\nauthor Manuel Riezebosch <mriezebosch@gmail.com> 1555269442 +0200\ncommitter Manuel Riezebosch <mriezebosch@gmail.com> 1555269442 +0200\n\nread commit from test\n" {
+		t.Fail()
 	}
-	io.Copy(os.Stdout, r)
-	r.Close()
 }
