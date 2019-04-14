ae710a6ef6cd3145a5366d1ed2b2d918d529e88a
Manuel Riezebosch <mriezebosch@gmail.com>
2019-04-14 21:17:22 +0200

    read commit from test

--- Main_test.go
+++ Main_test.go
@@ -1,6 +1,25 @@
 package Main
 
-import "testing"
+import (
+	"bytes"
+	"compress/zlib"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
-func TestMain(*testing.M) {
+func TestMain(t *testing.T) {
+	dat, err := ioutil.ReadFile(".git/objects/26/faf771e5f7c50d00a03b47e71b8df52ff8a7a7")
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := bytes.NewReader(dat)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		t.Error(err)
+	}
+	io.Copy(os.Stdout, r)
+	r.Close()
 }
