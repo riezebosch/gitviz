bb32bfa77b9125bf7f116286fae8575e45f7d17f
Manuel Riezebosch <mriezebosch@gmail.com>
2019-09-13 15:01:03 +0200

    refactor(routes): separate setup for routes

--- Main.go
+++ Main.go
@@ -1,97 +1,19 @@
 package main
 
 import (
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
 
-	"github.com/git-lfs/gitobj"
 	"github.com/pkg/browser"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
 )
 
 func main() {
-	repo, _ := gitobj.FromFilesystem(".git/objects", "")
-	defer repo.Close()
-
-	r := chi.NewRouter()
-	cors := cors.New(cors.Options{
-		AllowedOrigins: []string{"https://riezebosch.github.io"},
-		AllowedMethods: []string{"GET"},
-	})
-	r.Use(cors.Handler)
-
-	r.Get("/v1/graph/", func(w http.ResponseWriter, r *http.Request) {
-		output, _ := json.MarshalIndent(Visit(), "", "   ")
-		w.Write(output)
-	})
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		output, _ := Asset("html/index.html")
-		w.Write(output)
-	})
-
-	r.Route("/v1/objects", func(r chi.Router) {
-		r.Get("/blob/{id}", func(w http.ResponseWriter, r *http.Request) {
-			id, err := hex.DecodeString(chi.URLParam(r, "id"))
-			if err != nil {
-				return
-			}
-
-			o, err := repo.Blob(id)
-			if err != nil {
-				return
-			}
-
-			content, err := ioutil.ReadAll(o.Contents)
-			if err != nil {
-				return
-			}
-
-			w.Write(content)
-		})
-
-		r.Get("/tree/{id}", func(w http.ResponseWriter, r *http.Request) {
-			id, err := hex.DecodeString(chi.URLParam(r, "id"))
-			if err != nil {
-				return
-			}
-
-			o, err := repo.Tree(id)
-			if err != nil {
-				return
-			}
-
-			for _, entry := range o.Entries {
-				w.Write([]byte(fmt.Sprintf("%o %s %x\n", entry.Filemode, entry.Name, entry.Oid)))
-			}
-		})
-
-		r.Get("/commit/{id}", func(w http.ResponseWriter, r *http.Request) {
-			id, err := hex.DecodeString(chi.URLParam(r, "id"))
-			if err != nil {
-				return
-			}
-
-			o, err := repo.Commit(id)
-			if err != nil {
-				return
-			}
-
-			o.Encode(w)
-		})
-	})
-
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
 		panic(err)
 	}
 
 	browser.OpenURL(fmt.Sprintf("http://localhost:%v", listener.Addr().(*net.TCPAddr).Port))
-	panic(http.Serve(listener, r))
+	panic(http.Serve(listener, Routes()))
 }
---
+++ routes.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/go-chi/render"
+
+	"github.com/git-lfs/gitobj"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+)
+
+// Routes for graph and content
+func Routes() *chi.Mux {
+	r := chi.NewRouter()
+	cors := cors.New(cors.Options{
+		AllowedOrigins: []string{"https://riezebosch.github.io"},
+		AllowedMethods: []string{"GET"},
+	})
+	r.Use(cors.Handler)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		output, _ := Asset("html/index.html")
+		w.Write(output)
+	})
+
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/graph", getGraph)
+		r.Route("/objects", func(r chi.Router) {
+			r.Get("/blob/{id}", getBlob)
+			r.Get("/tree/{id}", getTree)
+			r.Get("/commit/{id}", getCommit)
+		})
+	})
+
+	return r
+}
+
+func getGraph(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, Visit())
+}
+
+func getBlob(w http.ResponseWriter, r *http.Request) {
+	repo, _ := gitobj.FromFilesystem(".git/objects", "")
+	defer repo.Close()
+
+	id, err := hex.DecodeString(chi.URLParam(r, "id"))
+	if err != nil {
+		return
+	}
+
+	o, err := repo.Blob(id)
+	if err != nil {
+		return
+	}
+
+	content, err := ioutil.ReadAll(o.Contents)
+	if err != nil {
+		return
+	}
+
+	w.Write(content)
+}
+
+func getTree(w http.ResponseWriter, r *http.Request) {
+	repo, _ := gitobj.FromFilesystem(".git/objects", "")
+	defer repo.Close()
+
+	id, err := hex.DecodeString(chi.URLParam(r, "id"))
+	if err != nil {
+		return
+	}
+
+	o, err := repo.Tree(id)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range o.Entries {
+		w.Write([]byte(fmt.Sprintf("%o %s %x\n", entry.Filemode, entry.Name, entry.Oid)))
+	}
+}
+
+func getCommit(w http.ResponseWriter, r *http.Request) {
+	repo, _ := gitobj.FromFilesystem(".git/objects", "")
+	defer repo.Close()
+
+	id, err := hex.DecodeString(chi.URLParam(r, "id"))
+	if err != nil {
+		return
+	}
+
+	o, err := repo.Commit(id)
+	if err != nil {
+		return
+	}
+
+	o.Encode(w)
+}
