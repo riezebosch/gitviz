830eb996038579d82e62cacbbf8841b0cee1e024
Manuel Riezebosch <mriezebosch@gmail.com>
2023-06-09 15:36:47 +0200

    fix(install): x86

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
-case $(uname -m) in
+#!/bin/bash
+
+set -e
+
+os=$(uname)
+arch=$(uname -m)
+
+case $arch in
     aarch64|arm64)
-    curl -L -o gitviz "https://github.com/riezebosch/gitviz/releases/latest/download/gitviz_$(uname)_arm64" 
-    ;; amd64)
-    curl -L -o gitviz "https://github.com/riezebosch/gitviz/releases/latest/download/gitviz_$(uname)_amd64"
-    ;; *) echo "$(uname -m) not supported by this script" exit 1
+        arch="arm64" 
+        ;;
+    x86_64|amd64)
+        arch="amd64"
+        ;;
+    *)
+        echo "$arch not supported by this script"
+        exit 1
+        ;;
 esac
 
+curl -L -o gitviz "https://github.com/riezebosch/gitviz/releases/latest/download/gitviz_${os}_${arch}"
 chmod +x gitviz
 sudo mv gitviz /usr/local/bin/gitviz
